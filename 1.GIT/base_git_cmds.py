b01e72ce64eb754a671ebe92a5d70a47e7439680
Eugen <eugen_test@example.com>
2023-06-21 13:43:07 +0300

    Добавил файл test5--

--- 1.GIT/base_git_cmds.py
+++ 1.GIT/base_git_cmds.py
@@ -4,11 +4,12 @@ import shutil
 import subprocess
 import os
 
-
 DIR_PATH = str(os.getcwd())
+
 
 def create(args):
     shutil.copy(args.src, args.dest)
+
 
 def check_git_status(path_check):
     os.chdir(path_check)
@@ -21,6 +22,7 @@ def clone(args):
     print(args.dest)
     result = subprocess.run(['git', 'clone', f'{args.src}', f'{args.dest}'])
     print(result.stdout)
+
 
 def create_branch(args):
     if check_git_status(args.dest) != 0:
@@ -38,13 +40,18 @@ def create_commit(args):
     result = subprocess.run(['git', 'commit', '-m', f'{args.msg}'])
     print(result.stdout)
 
+# def make_push(args):
+#     print(f'{args.msg}')
+#     result = subprocess.run(['git', 'add', '--all'])
+#     print(result.stdout)
+#     result = subprocess.run(['git', 'commit', '-m', f'{args.msg}'])
+#     print(result.stdout)
 
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(title='subcommands',
                                    description='valid subcommands',
                                    help='description')
-
 
 # -----------Команда клонирования репозитария в указанную директорию------
 clone_parser = subparsers.add_parser('clone', help='create new db')
@@ -57,19 +64,16 @@ clone_parser.set_defaults(func=clone)
 # -----------Создание новой ветки и переключение на неё-------------------
 create_branch_parser = subparsers.add_parser('create_branch', help='Create new branch & go into. Enter name of branch')
 create_branch_parser.add_argument(metavar='branch_name', dest='branch',
-                          help='branch')
+                                  help='branch')
 create_branch_parser.add_argument('--destination', '-d', dest='dest', default=DIR_PATH,
-                          help='destination', )
+                                  help='destination', )
 create_branch_parser.set_defaults(func=create_branch)
-
 
 # -----------Добавление файлов в индекс и создание коммита-----------------
 create_commit_parser = subparsers.add_parser('create_commit', help='Create new branch & go into. Enter name of branch')
 create_commit_parser.add_argument('--message', '-m', dest='msg', default='new_commit',
-                          help='Enter message.', )
+                                  help='Enter message.', )
 create_commit_parser.set_defaults(func=create_commit)
-
-
 
 if __name__ == '__main__':
     args = parser.parse_args()
