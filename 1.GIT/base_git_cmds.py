e5b06549467030e2b482666756576756d7a7a118
Eugen <eugen_test@example.com>
2023-06-22 16:49:01 +0300

    Домашнее задание #1 по теме GIT

--- 1.GIT/base_git_cmds.py
+++ 1.GIT/base_git_cmds.py
@@ -1,36 +1,56 @@
 #!/usr/bin/python3
 import argparse
-import shutil
 import subprocess
 import os
 
 DIR_PATH = str(os.getcwd())
 
-
-def create(args):
-    shutil.copy(args.src, args.dest)
-
-
 def check_git_status(path_check):
     os.chdir(path_check)
-    result = subprocess.run(['git', 'status'])
-    print(f'Результат выполнения комнады: {result.returncode}')
+    result = subprocess.run(['git', 'status'], stdout=subprocess.DEVNULL)
     return result.returncode
 
 
 def clone(args):
-    print(args.dest)
-    result = subprocess.run(['git', 'clone', f'{args.src}', f'{args.dest}'])
-    print(result.stdout)
+    try:
+        while os.path.exists(args.dest):
+            print('Такая директория уже существует. Создать клон не получиться. ')
+            args.dest = input("Укажите другой путь...\n")
+            print(f'Директория {args.dest}')
+        else:
+            print(f'Директория {args.dest}')
+            result = subprocess.run(['git', 'clone', f'{args.src}', f'{args.dest}'], encoding='utf-8',
+                                    stdout=subprocess.PIPE)
+    except:
+        name = args.src[args.src.rfind('/') + 1:-4]
+        print(name)
+        while name in os.listdir('.'):
+            print('Такая директория уже существует. Создать клон не получиться.')
+            name = input("Укажите другой путь...\n")
+            print(f'Директория {name}')
+        else:
+            print(f'Все норм, клонируем репозиторий в директорию {name}')
+            result = subprocess.run(['git', 'clone', f'{args.src}'], encoding='utf-8', stdout=subprocess.PIPE)
+            print(result.stdout)
+            print(result.returncode)
+
+            result = subprocess.run(['cd', '/home/eugen/git-test/111/Md-SA2-24-23'], encoding='utf-8', stdout=subprocess.PIPE)
+            print(result.stdout)
+            print(result.returncode)
+
 
 
 def create_branch(args):
     if check_git_status(args.dest) != 0:
-        print(f'Здесь нет репозитория. Не могу создать новую ветку.')
+        print(f'Указанная директория не содерижит репозиторий. Не могу создать новую ветку.')
     else:
-        result = subprocess.run(['git', 'checkout', '-b', f'{args.branch}'])
-        print(result.stdout)
-        print(f'Вы в новой ветке {args.branch}')
+        branch_list = subprocess.run(['git', 'branch'], encoding='utf-8', stdout=subprocess.PIPE).stdout.split()
+        if args.branch in branch_list:
+            print(f"Ветка с именем {args.branch} уже существует.")
+        else:
+            result = subprocess.run(['git', 'checkout', '-b', f'{args.branch}'], stdout=subprocess.DEVNULL)
+            # print(result.stdout)
+            print(f'Вы в новой ветке {args.branch}')
 
 
 def create_commit(args):
@@ -40,12 +60,12 @@ def create_commit(args):
     result = subprocess.run(['git', 'commit', '-m', f'{args.msg}'])
     print(result.stdout)
 
-# def make_push(args):
-#     print(f'{args.msg}')
-#     result = subprocess.run(['git', 'add', '--all'])
-#     print(result.stdout)
-#     result = subprocess.run(['git', 'commit', '-m', f'{args.msg}'])
-#     print(result.stdout)
+def make_push(args):
+    print(f'{args.msg}')
+    result = subprocess.run(['git', 'branch', '--show-current'], encoding='utf-8', stdout=subprocess.PIPE)
+    print(f'Мы находимся в ветке {result.stdout}')
+    result = subprocess.run(['git', 'push'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    print(f'Код выполнения задачи {result.returncode}')
 
 
 parser = argparse.ArgumentParser()
@@ -57,7 +77,7 @@ subparsers = parser.add_subparsers(title='subcommands',
 clone_parser = subparsers.add_parser('clone', help='create new db')
 clone_parser.add_argument(metavar='db-filename', dest='src',
                           default='DFLT_DB_NAME', help='source')
-clone_parser.add_argument('--destination', '-d', dest='dest', default=DIR_PATH,
+clone_parser.add_argument('--destination', '-d', dest='dest', #default=DIR_PATH,
                           help='destination', )
 clone_parser.set_defaults(func=clone)
 
@@ -74,6 +94,10 @@ create_commit_parser = subparsers.add_parser('create_commit', help='Create new b
 create_commit_parser.add_argument('--message', '-m', dest='msg', default='new_commit',
                                   help='Enter message.', )
 create_commit_parser.set_defaults(func=create_commit)
+
+# -----------Отправка ветки на удаленный репозиторий-----------------------
+make_push_parser = subparsers.add_parser('make_push', help='Create new branch & go into. Enter name of branch')
+make_push_parser.set_defaults(func=make_push)
 
 if __name__ == '__main__':
     args = parser.parse_args()
